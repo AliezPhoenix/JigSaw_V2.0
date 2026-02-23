9a1d0c4a21178145e9c3104276231c74e0ab3bf9
Alizephoenix_home_base <373260087@qq.com>
2026-02-23 22:49:25 +0800

    Refactor template validity test connections and enable ROI creation buttons for both dry and transfer templates

--- main_window.py
+++ main_window.py
@@ -116,12 +116,12 @@ class MainWindow(main_window_ui.Ui_MainWindow, QMainWindow):
 
         self.btn_create_new_template.clicked.connect(lambda: self.create_new_tmepalte("dry"))
         self.btn_create_new_template_transfer.clicked.connect(lambda: self.create_new_tmepalte("transfer"))
-        self.pushButton_tempaltematch.clicked.connect(self.dry_template_validity_test)
-        # self.pushButton_transfer_template_match.clicked.connect(self.transfer_template_validity_test)
+        self.pushButton_tempaltematch.clicked.connect(lambda: self.template_validity_test("dry"))
+        self.pushButton_transfer_template_match.clicked.connect(lambda: self.template_validity_test("transfer"))
         self.pushButton_current_image_select_dry.clicked.connect(lambda: self.load_current_image("dry"))
         self.pushButton_current_image_select_transfer.clicked.connect(lambda: self.load_current_image("transfer"))
-        # self.pushButton_create_checkable_roi_dry.clicked.connect(lambda: self.create_search_roi("dry"))
-        # self.pushButton_create_checkable_roi_transfer.clicked.connect(lambda: self.create_search_roi("transfer"))
+        self.pushButton_create_checkable_roi_dry.clicked.connect(lambda: self.create_search_roi("dry"))
+        self.pushButton_create_checkable_roi_transfer.clicked.connect(lambda: self.create_search_roi("transfer"))
         # self.pushButton_fulltray_save.clicked.connect(self.save_fulltray_params)
         # self.pushButton_fulltray_select_model.clicked.connect(self.select_fulltray_model)
         # self.pushButton_fulltray_set_roi.clicked.connect(self.create_fulltray_grid_roi)
@@ -578,6 +578,8 @@ class MainWindow(main_window_ui.Ui_MainWindow, QMainWindow):
     def template_validity_test(self,station):
         pass
 
+    def create_search_roi(self,station):
+        pass
 
 
 
